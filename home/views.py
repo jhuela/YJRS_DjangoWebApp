a1aed88edf1a9a9ff1aec6f2d9f65cd838df375c
pgnaguit <pgmnaguit.student@ua.edu.ph>
2026-09-15 22:19:42 +0800

    Complete Module 03 CRUD implementation

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+def welcome(request):
+    return render(request, "home/welcome.html")
+
+
+def dynamic_form(request):
+    if request.method == "POST":
+        student_name = request.POST.get("student_name")
+        program = request.POST.get("program")
+
+        context = {
+            "student_name": student_name,
+            "program": program,
+        }
+
+        return render(request, "home/welcome.html", context)
+
+    return render(request, "home/dynamic_form.html")
